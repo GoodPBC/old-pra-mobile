47f00345ed44912105157f1b1d57169ea1453e82
Jameson Knoll <j.knoll621@gmail.com>
2016-12-08 03:03:42 +0800

    add filters for requests and sort requests by importance

--- src/serviceRequests/components/ServiceRequestList.js
+++ src/serviceRequests/components/ServiceRequestList.js
@@ -1,10 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 
-import { ListView } from 'react-native';
+import { View, StyleSheet, ListView } from 'react-native';
 
 import Separator from '../../shared/components/Separator';
 
 import ServiceRequestListItem from './ServiceRequestListItem';
+
+import { Button } from '../../shared';
 
 export default class ServiceRequestList extends Component {
   constructor(props) {
@@ -12,7 +14,10 @@ export default class ServiceRequestList extends Component {
 
     const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 
-    this.state = { dataSource: ds.cloneWithRows(props.serviceRequests) };
+    this.state = { 
+      dataSource: ds.cloneWithRows(props.serviceRequests),
+      displayServiceRequests: 'active' 
+    };
 
     this._selectServiceRequest = this._selectServiceRequest.bind(this);
 
@@ -22,10 +27,35 @@ export default class ServiceRequestList extends Component {
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.serviceRequests) {
+      this._sortServiceRequests(nextProps.serviceRequests);
       this.setState({
         dataSource: this.state.dataSource.cloneWithRows(nextProps.serviceRequests),
       });
     }
+  }
+
+  _sortServiceRequests(serviceRequests) {
+    
+    function compare(a, b){
+      if (statusMap[a] < statusMap[b]) {
+        return -1;
+      }
+      if (statusMap[a] > statusMap[b]) {
+        return 1;
+      }
+      return 0;
+    }
+
+    let statusMap = {
+      on_site: 0,
+      in_the_field: 1,
+      visit_complete: 2,
+      closed: 3,
+      in_process: 4,
+      assigned: 5
+    };
+
+    return serviceRequests.sort(compare);
   }
 
   _selectServiceRequest(serviceRequest) {
@@ -38,12 +68,29 @@ export default class ServiceRequestList extends Component {
   }
 
   _renderRow(rowData) {
-    return (
-      <ServiceRequestListItem
-        serviceRequest={rowData}
-        selectServiceRequest={this._selectServiceRequest}
-      />
-    );
+    if (this.state.displayServiceRequests === 'active') {
+      if (rowData.status === 'in_the_field' || rowData.status === 'on_site') {
+        return (
+          <ServiceRequestListItem
+            serviceRequest={rowData}
+            selectServiceRequest={this._selectServiceRequest}
+          />
+        );
+      } else {
+        return null;
+      }
+    } else {
+      if (rowData.status === 'visit_complete'|| rowData.statue == 'closed') {
+        return (
+          <ServiceRequestListItem
+            serviceRequest={rowData}
+            selectServiceRequest={this._selectServiceRequest}
+          />
+        );
+      } else {
+        return null;
+      }
+    }
   }
 
   _renderSeparator(sectionID, rowID) {
@@ -54,12 +101,18 @@ export default class ServiceRequestList extends Component {
 
   render() {
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this._renderRow}
-        renderSeparator={this._renderSeparator}
-        enableEmptySections
-      />
+      <View style={styles.container}>
+        <View style={styles.buttonRow}>
+          <Button style={styles.button} onPress={ () => this.setState({ displayServiceRequests: 'active' }) }>Active</Button>
+          <Button style={styles.button} onPress={ () => this.setState({ displayServiceRequests: 'inactive' }) }>Closed</Button>
+        </View>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderRow}
+          renderSeparator={this._renderSeparator}
+          enableEmptySections
+        />
+      </View>
     );
   }
 }
@@ -71,3 +124,18 @@ ServiceRequestList.propTypes = {
   serviceRequests: PropTypes.array.isRequired,
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  button: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'white'
+  }
+});
