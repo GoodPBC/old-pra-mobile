1fd0aba9f63d456d9b07c596697cf67154b1ad2b
Jameson Knoll <j.knoll621@gmail.com>
2017-02-21 03:10:25 +0800

    additional filtering for urgent service requests

--- src/serviceRequests/components/MyRequestsScreen.js
+++ src/serviceRequests/components/MyRequestsScreen.js
@@ -2,15 +2,43 @@ import React, { Component, PropTypes } from 'react';
 
 import { StyleSheet, View } from 'react-native';
 
+import moment from 'moment';
+
 import ServiceRequestList from '../containers/ServiceRequestList';
 import EmptyServiceRequestList from './EmptyServiceRequestList';
 
 export default class MyRequestsScreen extends Component {
+  
+  constructor(props) {
+    super(props)
+
+    this.filterServiceRequestsByUrgency = this.filterServiceRequestsByUrgency.bind(this)
+  }
+
   componentWillMount() {
     this.props.fetchServiceRequests();
+    if (this.props.serviceRequests) {
+      this.filterServiceRequestsByUrgency();
+    }
+  }
+
+  filterServiceRequestsByUrgency(){
+    let urgentServiceRequests = this.props.serviceRequests.map( (serviceRequest) => {
+      if (serviceRequest.status === "in_the_field") {
+        let now = new Date();
+        let one_hour = 60 * 60 * 1000;
+        let sr_time = moment(serviceRequest.updated_at).valueOf();
+        if (now - sr_time > one_hour && serviceRequest.has_alerted === false) {
+          return serviceRequest
+        }
+      }
+    }).filter(Boolean)
+
+    console.log(urgentServiceRequests)
   }
 
   render() {
+    console.log(this.props.serviceRequests);
     return (
       <View style={styles.container}>
       {this.props.serviceRequests === undefined || this.props.serviceRequests.length == 0 ?
