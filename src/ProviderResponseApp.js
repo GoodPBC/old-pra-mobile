7ecc6e743b92900072ec03835e89a57f8d6bd787
Kristopher Kelly <kristopherbkelly@gmail.com>
2017-03-13 17:24:02 -0400

    PRA-143 Add JS exception handling for Crashlytics

--- src/ProviderResponseApp.js
+++ src/ProviderResponseApp.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import crashlytics from 'react-native-fabric-crashlytics';
 import App from './app/containers/App';
 import configureStore from './store';
+
+crashlytics.init();
 
 const store = configureStore();
 
