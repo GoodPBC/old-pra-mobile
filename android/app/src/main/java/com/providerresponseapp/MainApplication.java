42b4a3f0db511ce543f9cd78dcc52d8522c63ca4
Euibin Cheon <euibinc@gmail.com>
2018-02-02 15:13:07 -0500

    Install react-native-push-notification

--- android/app/src/main/java/com/providerresponseapp/MainApplication.java
+++ android/app/src/main/java/com/providerresponseapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.providerresponseapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.instabug.reactlibrary.RNInstabugReactnativePackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -43,6 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+        new ReactNativePushNotificationPackage(),
     		new RNInstabugReactnativePackage.Builder(BuildConfig.INSTABUG_TOKEN_ANDROID, MainApplication.this)
 							.setInvocationEvent("button")
 							.setPrimaryColor("#DE6053")
