b67c99c6bc70212ea634e72c8ed885a144f82b64
Euibin Cheon <euibinc@gmail.com>
2018-02-06 11:01:53 -0500

    Update test

--- jest/setup.js
+++ jest/setup.js
@@ -1,3 +1,22 @@
 jest.mock('react-native-fabric-crashlytics', () => ({
   init: jest.fn(),
 }));
+
+jest.mock('instabug-reactnative', () => ({
+  setPromptOptionsEnabled: jest.fn(),
+  startWithToken: jest.fn(),
+  invocationEvent: {
+    shake: 'shake',
+  },
+}));
+
+jest.mock('react-native-vector-icons/FontAwesome', () => 'FontAwesome');
+
+jest.mock('react-native-code-push', () => ({
+  CheckFrequency: {
+    ON_APP_RESUME: 'ON_APP_RESUME',
+  },
+  InstallMode: {
+    IMMEDIATE: 'IMMEDIATE',
+  },
+}))
