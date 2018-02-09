d9f8c45fe05046a49dc2e06caf364347624b8a36
Euibin Cheon <euibinc@gmail.com>
2018-02-09 16:09:07 -0500

    Initial test

--- jest/setup.js
+++ jest/setup.js
@@ -12,11 +12,17 @@ jest.mock('instabug-reactnative', () => ({
 
 jest.mock('react-native-vector-icons/FontAwesome', () => 'FontAwesome');
 
-jest.mock('react-native-code-push', () => ({
-  CheckFrequency: {
-    ON_APP_RESUME: 'ON_APP_RESUME',
-  },
-  InstallMode: {
-    IMMEDIATE: 'IMMEDIATE',
-  },
-}))
+jest.mock('react-native-push-notification', () => ({
+    configure: jest.fn(),
+    onRegister: jest.fn(),
+    onNotification: jest.fn(),
+    addEventListener: jest.fn(),
+    requestPermissions: jest.fn(),
+}));
+
+jest.mock('react-native-code-push', () => {
+  const codePush = () => app => app;
+  codePush.InstallMode = {};
+  codePush.CheckFrequency = {};
+  return codePush;
+});
