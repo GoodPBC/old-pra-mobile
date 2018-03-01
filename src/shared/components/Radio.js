f276d5ec7f79f605463c6e1264ff30225db8f6bd
Euibin Cheon <cheone@dss.nyc.gov>
2018-03-01 18:16:14 -0500

    PM-68

--- src/shared/components/Radio.js
+++ src/shared/components/Radio.js
@@ -3,10 +3,19 @@ import { Image, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import checkedCircleImage from './img/select-radio-active.png';
 import uncheckedCircleImage from './img/select-radio-inactive.png';
+import checkedCircleImageOnBlue from './img/select-radio-active-on-blue.png';
+import uncheckedCircleImageOnBlue from './img/select-radio-inactive-on-blue.png';
 
-export default function Radio({ checked }) {
+export default function Radio({ checked, blueBackground }) {
+  let source;
+  if (blueBackground) {
+    source = checked ? checkedCircleImageOnBlue : uncheckedCircleImageOnBlue;
+  } else {
+    source = checked ? checkedCircleImage : uncheckedCircleImage;
+  }
+
   return (
-    <Image source={checked ? checkedCircleImage : uncheckedCircleImage} style={styles.image} />
+    <Image source={source} style={styles.image} />
   );
 }
 
